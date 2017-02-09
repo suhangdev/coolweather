62b209cd1dfe8501847fb433b879aef39cd4ab47
suhang <17816876697@163.com>
2017-02-09 16:49:01 +0800

    加入天气布局

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by suhang on 2017/2/9.
+ */
+
+public class AQI {
+    public AQIcity city;
+    public class AQIcity{
+        public String aqi;
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -0,0 +1,19 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by suhang on 2017/2/9.
+ */
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+    @SerializedName("id")
+    public String weatherId;
+    public Update update;
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
